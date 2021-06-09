5227d251ebe2621eafbf75cb4988d700c03a20c4
unknown <Administrateur@A-DevFixe009.myapollossc.com>
2021-06-09 10:48:11 +0200

    Update dependencies

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 const ResumeParser = require('./src');
 
-// const fileDir = process.cwd() + '/files/';
-// ResumeParser
-//   .parseResumeFile(fileDir + 'resume.doc', fileDir + 'compiled') //input file, output dir
-//   .then(file => {
-//     console.log("Yay! " + file);
-//   })
-//   .catch(error => {
-//     console.log('parseResume failed');
-//     console.error(error);
-//   });
+ const fileDir = process.cwd() + '/files/';
+ ResumeParser
+   .parseResumeFile(fileDir + 'resume.pdf', fileDir + 'compiled') //input file, output dir
+   .then(file => {
+     console.log("Yay! " + file);
+   })
+   .catch(error => {
+     console.log('parseResume failed');
+     console.error(error);
+   });
 
 ResumeParser.parseResumeUrl('http://www.mysite.com/resume.txt') // url
   .then(data => {
